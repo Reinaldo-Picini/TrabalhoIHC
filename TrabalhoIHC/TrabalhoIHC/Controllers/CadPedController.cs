66746cec10e30108d6617dc566270393764d1579
Reinaldo\ReinaldoPicini <lpreinaldo@gmail.com>
2017-11-18 15:38:16 -0200

    Criado as Classes CadPet, CadUsuario, a Controller CadPet e a View ListarPer

---
+++ TrabalhoIHC/TrabalhoIHC/Controllers/CadPedController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrabalhoIHC.Controllers
+{
+    public class CadPedController : Controller
+    {
+        // GET: CadPed
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TrabalhoIHC/TrabalhoIHC/Models/CadPet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrabalhoIHC.Models
+{
+    public class CadPet
+    {
+        public int PetId { get; set; }
+        public string Fotos { get; set; }
+        public string Nome { get; set; }
+        public string Raca { get; set; }
+        public DateTime Idade { get; set; }
+        public string Vacina { get; set; }
+        public string Porte { get; set; }
+        public string Sexo { get; set; }
+        public string Temperamento { get; set; }
+        public string Descricao { get; set; }
+    }
+}
