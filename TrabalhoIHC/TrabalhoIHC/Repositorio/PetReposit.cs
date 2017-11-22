cde8ab35bd93f156898f4e8e6f04ef7c3b56ac12
jroberto288 <jose.roberto@infouniron.com>
2017-11-21 22:31:17 -0300

    Criado repositorio

---
+++ TrabalhoIHC/TrabalhoIHC/Repositorio/PetReposit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MeetingsPets_Original.Models;
+
+namespace MeetingsPets_Original.Repositorio
+{
+    public class PetReposit
+    {
+        public static IList<Pet> Pet = new List<Pet>();
+
+    }
+}
