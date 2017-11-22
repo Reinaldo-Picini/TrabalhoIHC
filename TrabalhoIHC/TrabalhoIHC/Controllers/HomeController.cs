5094f4740ad06529da4534cad8b238da1a90a1fb
josuematador <Seila42>
2017-11-21 22:28:14 -0300

    Ajustado a home controle.

--- TrabalhoIHC/TrabalhoIHC/Controllers/HomeController.cs
+++ TrabalhoIHC/TrabalhoIHC/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace MeetingsPets_Original.Controllers
+namespace TrabalhoIHC.Controllers
 {
     public class HomeController : Controller
     {
