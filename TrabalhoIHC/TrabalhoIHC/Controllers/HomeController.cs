f207d64f88cb068c365562218e6e631c30aa1674
josuematador <Seila42>
2017-11-21 22:23:29 -0300

    Criado a tela inicial e a home controlle.

---
+++ TrabalhoIHC/TrabalhoIHC/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MeetingsPets_Original.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Ajuda()
+        {
+
+            return View();
+        }
+    }
+}
