4c253da18f1114a75c0e05498b3d6488c0ad9e75
EDMILSON\edmilson <edmilson.barbosa@infouniron.com>
2017-11-21 21:45:20 -0300

    Feito a Usuariocontroller e implementado view CadUsuario

---
+++ TrabalhoIHC/TrabalhoIHC/Controllers/UsuarioController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrabalhoIHC.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult CadUsuario()
+        {
+            ViewBag.Message = "Cadastro de Usuários";
+            return View();
+        }
+    }
+}
